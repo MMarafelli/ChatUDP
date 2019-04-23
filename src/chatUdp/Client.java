a054cd33115b6158d1e4f648ae73c2c2ca093c8f
Matheus Marafelli <mmarafelli@outlook.com>
2019-04-23 01:00:32 -0300

    Adição de timestamp

--- src/chatUdp/Client.java
+++ src/chatUdp/Client.java
@@ -1,17 +1,17 @@
 package chatUdp;
 
-/************************
-*  Matheus Marafeli     *
-*         IFTM          *
-* Sistemas Distribuídos *
-*                       *
-*      Client           *
-*************************/
-
+/**
+ * **********************
+ * Matheus Marafeli * IFTM * Sistemas Distribuídos * * Client *
+ * ***********************
+ */
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -26,6 +26,11 @@ public class Client extends Thread {
     public static void main(String[] args) {
 
         try {
+
+            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+            Date hora = Calendar.getInstance().getTime();
+            String dataFormatada = sdf.format(hora);
+
             DatagramSocket s = new DatagramSocket();
             InetAddress destino = InetAddress.getByName("localhost");
             BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
@@ -47,7 +52,7 @@ public class Client extends Thread {
                 textoMensagem = teclado.readLine();
 
                 if (!textoMensagem.equals("")) {
-                    envio = nome + " disse: " + textoMensagem;
+                    envio = nome + " [" + dataFormatada + "] disse: " + textoMensagem;
                 } else {
                     envio = textoMensagem;
                 }
