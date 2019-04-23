22f3f8f8f5af17efa20877757ddee6fc76c99a27
Matheus Marafelli <mmarafelli@outlook.com>
2019-04-22 22:31:46 -0300

    Chat UDP

---
+++ src/chatUdp/Servidor.java
@@ -0,0 +1,72 @@
+package chatUdp;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+ /************************
+ * Matheus Marafeli      * 
+ *      IFTM             * 
+ * Sistemas Distribuídos * 
+ *                       *
+ *       Sevidor         *
+ *************************/
+
+
+public class Servidor extends Thread {
+
+    private DatagramSocket ds;
+    private List<Integer> portList;
+    private List<InetAddress> addressList;
+
+    public static void main(String args[]) throws Exception {
+        Servidor s = new Servidor();
+        s.start();
+    }
+
+    public Servidor() {
+        try {
+            ds = new DatagramSocket(1313);
+            addressList = new ArrayList<>();
+            portList = new ArrayList<>();
+        } catch (SocketException ex) {
+            System.out.println(ex.getMessage() + " Local: S ervidor Chat UDP init");
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+
+            DatagramSocket s = new DatagramSocket(1314);
+            System.out.println("Waiting for connection......");
+
+            DatagramPacket received = new DatagramPacket(new byte[1024], 1024);
+
+            while (true) {
+                s.receive(received);
+
+                if (!portList.contains(received.getPort())) {
+                    portList.add(received.getPort());
+                    addressList.add(received.getAddress());
+                }
+
+                for (int i = 0; i < portList.size(); i++) {
+                    if (portList.get(i) != received.getPort()) {
+                        DatagramPacket resp = new DatagramPacket(received.getData(), received.getLength(), addressList.get(i), portList.get(i));
+                        s.send(resp);
+                    }
+                }
+                System.out.println();
+            }
+
+        } catch (IOException ioex) {
+            System.out.println(ioex.getMessage());
+        }
+    }
+}
